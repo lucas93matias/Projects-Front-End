532f3d4b9bec770b79da27f9bf3213f59f27868e
lucas93matias <156041583+lucas93matias@users.noreply.github.com>
2024-01-10 21:30:28 -0300

    Load da maquina
    aqui onde apliquei meus conhecimentos de JS em IF e ELSE

---
+++ index.js
@@ -0,0 +1,11 @@
+const idade = prompt("Informe sua idade:")
+
+if (idade > 18) {
+    alert("Você é menor de idade")
+} else if(idade > 12) {
+    alert ("Vcoê é menor de idade")
+    } else if (idade > 4) {
+        alert("Você é criança")
+    } else {
+        alert ("Você deve ir dormir pia!")
+    }
