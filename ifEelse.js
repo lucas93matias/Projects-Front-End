3f7fc8fe523deb4ddcc714893392977af4fb4215
lucas93matias <156041583+lucas93matias@users.noreply.github.com>
2024-01-11 21:31:46 -0300

    Aula de JS - IF e ELSE com exemplos
    aprendi a utilizar IF e ELSE com exemplos práticos de uso no console, porém pode ser utilizado guardando valores pelo buscador.

---
+++ ifEelse.js
@@ -0,0 +1,10 @@
+let energia=100
+let clima="chovendo"
+
+if(energia > 100 && clima== "sol"){
+    console.log("Vou pedalar!")
+}else{
+    console.log("Vou estudar!")
+}
+
+console.log("Fim do programa")
