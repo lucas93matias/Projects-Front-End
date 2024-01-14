6c9c25e8e42a97cd402813169ffbd0b8c67a72cf
Lucas Matias <156041583+lucas93matias@users.noreply.github.com>
2024-01-14 18:48:22 -0300

    Aula de SWITCH
    Nesta aula aprendi o que é o SWITCH, com ele é possível criar condicionais que avaliam, sempre utilizar na escrita em cada condição o " : " (dois pontos). Ele avalia as alternativas.

---
+++ switch.js
@@ -0,0 +1,12 @@
+const resultado = prompt("Escolha uma alternativa:\na)\nb)")
+
+switch (resultado) {
+  case "a":
+  alert("O resultado é 'a'")
+  break
+  case "b":
+    alert("O resultado é 'b'")
+    default:
+      alert ("Finalizando")
+    break
+}
