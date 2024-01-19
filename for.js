e369e0b54903137b763ee82bfc6aa0a0524e8493
Lucas Matias <156041583+lucas93matias@users.noreply.github.com>
2024-01-18 21:19:06 -0300

    aula 28 - Estrutura de repetição: for
    Nesta aula aprendi e pudi aplicar meu conhecimento na estrutura for, que se baseia em repetir uma estrutura numa quantidade especifica. Se baseia em três pilares: inicialização ; condição ; finalização. Sempre com " ; ".
    Cuidado para não criar um loop infinito com o for.

---
+++ for.js
@@ -0,0 +1,10 @@
+// inicialização ; condição ; finalização
+let indice = 0
+
+for (
+    alert ("Inicializando o for");
+    indice <= 5;
+    indice++
+) {
+    alert("Indice = " + indice)
+} window.alert("Fim da aula")
