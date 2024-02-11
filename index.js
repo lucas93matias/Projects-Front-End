1a255969ced53cb64cd346fdd48a4582e0d91334
Lucas Matias <156041583+lucas93matias@users.noreply.github.com>
2024-02-11 10:18:27 -0300

    Eventos no JS - Aula 2
    Nesta aula pude adicionar eventos numa pagina de HTML junto com o javaScript. Criando assim mais interação do usuário e criando assim os chamados eventos.

--- index.js
+++ index.js
@@ -1,17 +1,15 @@
-const turista = prompt("E aí, turista! Qual é o seu nome?")
-let cidades = ""
-let contagem = 0
-
-let continuar = prompt("Você visitou alguma cidade? (Sim/Não)")
-
-while (continuar === "Sim") {
-  let cidade = prompt("Qual é o nome da cidade visitada?")
-  cidades += " - " + cidade + "\n"
-  contagem++
-  continuar = prompt("Você visitou alguma outra cidade? (Sim/Não)")
+function onMouseEnter() {
+    console.log('mouse enter')
 }
-alert(
-  "Turista: " + turista +
-  "\nQuantidade de cidades visitadas: " + contagem +
-  "\nCidades visitadas:\n" + cidades
-)
+
+function onFocus() {
+    console.log('onFocus')
+}
+
+function onBlur() {
+    console.log('onBlur')
+}
+
+function onclick() {
+    console.log('onclick')
+}
