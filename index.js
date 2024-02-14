487ebdb16cb5ecc795211695d7d4e89f296352e0
Lucas Matias <156041583+lucas93matias@users.noreply.github.com>
2024-02-14 12:11:32 -0300

    Inserindo valores do DOM no JS
    neste exercício adicionei valores do DOM no JavaScript, através de um formulário de registo de HTML, onde até adicionamos uma mensagem de alerta de verificação de senha iguais ou desiguais.

--- index.js
+++ index.js
@@ -1,52 +1,11 @@
-function addContact() {
-    const contactSection = document.getElementById('contacts-list')
+function register(element) {
+    const username = element.children.username.value
+    const password = element.children.password.value
+    const passwordConfirmation = element.children.passwordConfirmation.value
 
-    const h3 = document.createElement('h3')
-    h3.innerText = "Contato "
-
-    const ul = document.createElement('ul')
-
-    const nameLi = document.createElement('li')
-    nameLi.innerText = "Nome "
-    const nameInput = document.createElement('input')
-    nameInput.type = 'text'
-    nameInput.name = 'fullname'
-    nameLi.appendChild(nameInput)
-    ul.appendChild(nameLi)
-    ul.appendChild(document.createElement('br'))
-
-    const phoneLi = document.createElement('li')
-    phoneLi.innerText = "Telefone"
-    const phoneInput = document.createElement('input')
-    nameInput.type = 'text'
-    nameInput.name = 'fullphone'
-    phoneLi.appendChild(phoneInput)
-    ul.appendChild(phoneLi)
-    ul.appendChild(document.createElement('br'))
-    
-
-    const addressLi = document.createElement('li')
-    addressLi.innerHTML = '<label for="address">Endereço: </label>'
-    const addressInput = document.createElement('input')
-    addressInput.type = 'text'
-    nameInput.name = 'address'
-    addressInput.id = 'address'
-    addressLi.appendChild(addressInput)
-    ul.appendChild(addressLi)
-    ul.appendChild(document.createElement('br'))
-
-    contactSection.append(h3, ul)
-
-}
-
-function removeContact() {
-    const contactSection = document.getElementById('contacts-list')
-
-    const titles = document.getElementsByTagName('h3')
-    const contacts = document.getElementsByTagName('ul')
-
-    contactSection.removeChild(titles[titles.length - 1]);
-    contactSection.removeChild(contacts[contacts.length - 1]);
-
-
+    if (password === passwordConfirmation) {
+        alert("Usuário " + username + " registrado!")
+    } else {
+        alert("As senhas não conferem! Tente novamente!")
+    }
 }
