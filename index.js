ea5e3b61487f74212f6427aa0e8cce338b1bb249
Lucas Matias <156041583+lucas93matias@users.noreply.github.com>
2024-02-14 19:59:02 -0300

    Working with events on JS
    Nesta aprendi outra forma de adicionar arquivo js no HTML
    atraves do:
     <script src= "./index.js" defer></script>
    com esse forma "defer" o buscador lê o jsvaScript depois de ler o html

--- index.js
+++ index.js
@@ -1,11 +1,21 @@
-function register(element) {
-    const username = element.children.username.value
-    const password = element.children.password.value
-    const passwordConfirmation = element.children.passwordConfirmation.value
+function register(ev) {
+    const sectionElement = document.querySelector('section'); // Seleciona a seção que contém os campos de entrada
+    const username = sectionElement.querySelector('#username').value; // Obtém o valor do campo de entrada do nome de usuário
+    const password = sectionElement.querySelector('#password').value; // Obtém o valor do campo de entrada da senha
+    const passwordConfirmation = sectionElement.querySelector('#passwordConfirmation').value; // Obtém o valor do campo de entrada de confirmação de senha
 
     if (password === passwordConfirmation) {
-        alert("Usuário " + username + " registrado!")
+        alert('Usuário ' + username + ' registrado!');
     } else {
-        alert("As senhas não conferem! Tente novamente!")
+        alert('As senhas não são iguais! Tente novamente!');
     }
+}
+
+const button = document.getElementById('register-button');
+
+button.addEventListener('click', register);
+
+function removeListener() {
+    button .removeEventListener('click', register)
+    alert('Event Removed')
 }
